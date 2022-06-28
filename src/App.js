8840d64306eb30b4b6e33d9e3b37bc5ca96554ae
Akindele Ayorinde Emmanuel <73663718+Hayor4real@users.noreply.github.com>
2022-06-28 21:11:20 +0200

    added the cart functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Nav from './Components/Nav';
 import HomeScreen from './screens/HomeScreen';
-
+import Cart from './Components/Cart';
 function App() {
   return (
     <div className='App'>
       <Nav />
-
+      <Cart />
       <HomeScreen />
     </div>
   );
