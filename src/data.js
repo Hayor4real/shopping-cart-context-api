4c0987b0ac98c8bef0b985ddf4c17deba945b911
Akindele Ayorinde Emmanuel <73663718+Hayor4real@users.noreply.github.com>
2022-06-28 16:42:44 +0200

    added the image files

---
+++ src/data.js
@@ -0,0 +1,157 @@
+const products = [
+  {
+    _id: '1',
+    name: 'nike Slim shirt',
+    image: '/images/p1.jpg',
+
+    price: 200,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Adidas Fit Shirt',
+    image: '/images/p2.jpg',
+
+    price: 300,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'nike-slim-pant',
+    image: '/images/p3.jpg',
+
+    price: 325,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Adidas Fit Pant',
+    image: '/images/p4.jpg',
+
+    price: 265,
+    rating: 5,
+    numReviews: 15,
+  },
+  {
+    _id: '5',
+    name: 'Cartoon Astronuat T-Shirts',
+    image: 'images/f1.jpg',
+
+    price: 180,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Cartoon flower T-Shirts',
+    image: '/images/f3.jpg',
+
+    price: 195,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: '7',
+    name: 'Cartoon designer T-Shirts',
+    image: '/images/f4.jpg',
+
+    price: 495,
+    rating: 4,
+    numReviews: 17,
+  },
+  {
+    _id: '8',
+    name: 'Cartoon jacket T-Shirt',
+    image: '/images/f6.jpg',
+
+    price: 205,
+    rating: 4,
+    numReviews: 19,
+  },
+  {
+    _id: '9',
+    name: 'Cartoon 3 quater',
+    image: '/images/f7.jpg',
+
+    price: 250,
+    rating: 4,
+    numReviews: 13,
+  },
+  {
+    _id: '10',
+    name: 'Cartoon slim fit T-shirt',
+    image: '/images/n1.jpg',
+
+    price: 280,
+    rating: 4,
+    numReviews: 15,
+  },
+  {
+    _id: '11',
+    name: 'Cartoon Ash slim fit T-Shirt',
+    image: '/images/n2.jpg',
+
+    price: 280,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: '12',
+    name: 'Cartoon white slim fit T-Shirt',
+    image: '/images/n3.jpg',
+
+    price: 250,
+    rating: 4,
+    numReviews: 19,
+  },
+  {
+    _id: '13',
+    name: 'Cartoon  slim fit T-Shirt',
+    image: '/images/n4.jpg',
+
+    price: 285,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: '14',
+    name: 'Cartoon blue slim fit T-Shirt',
+    image: '/images/n5.jpg',
+
+    price: 300,
+    rating: 4,
+    numReviews: 18,
+  },
+  {
+    _id: '15',
+    name: 'Cartoon short trouser',
+    image: '/images/n6.jpg',
+
+    price: 260,
+    rating: 4,
+    numReviews: 21,
+  },
+  {
+    _id: '16',
+    name: 'Cartoon brawn slim fit T-Shirt',
+    image: '/images/n7.jpg',
+
+    price: 190,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: '17',
+    name: 'Cartoon purple slim fit T-Shirt',
+    image: '/images/n8.jpg',
+
+    price: 190,
+    rating: 4,
+    numReviews: 11,
+  },
+];
+
+export default products;
