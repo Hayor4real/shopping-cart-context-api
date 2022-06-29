b360dfc14418399c9a559599cbcd27fd0e0c0158
Akindele Ayorinde Emmanuel <73663718+Hayor4real@users.noreply.github.com>
2022-06-29 18:57:55 +0200

    Added the types js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,6 +1,10 @@
 import './Nav.css';
+import { useContext } from 'react';
+import CartContext from '../context/cart/CartContext';
 
 const Nav = () => {
+  const { cartItems, showHideCart } = useContext(CartContext);
+
   return (
     <nav>
       <div className='nav__left'>Store</div>
@@ -10,9 +14,19 @@ const Nav = () => {
           <i className='fas fa-search' />
         </div>
       </div>
+
       <div className='nav__right'>
         <div className='cart__icon'>
-          <i className='fa fa-shopping-cart' aria-hidden='true' />
+          <i
+            className='fa fa-shopping-cart'
+            aria-hidden='true'
+            onClick={showHideCart}
+          />
+          {cartItems.length > 0 && (
+            <div className='item__count'>
+              <span>{cartItems.length}</span>
+            </div>
+          )}
         </div>
       </div>
     </nav>
